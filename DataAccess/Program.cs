c9c858b0bc3f2664e864b4547828331e0e72570e
dertnius <drivas.yanez@gmail.com>
2018-02-16 16:26:07 +0100

    Added solution with netcore 2.0 and ef 2.0

---
+++ DataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aspnetcore_ef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
